e96324867c506e35524309f5f7bc72e55521dc82
simoneluca00 <si.zaino@libero.it>
2022-05-05 10:57:48 +0200

    Correction

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
-<!-- 
+<?php
+/* 
     Creare una variabile con un paragrafo di testo a vostra scelta.
     Stampare a schermo il paragrafo e la sua lunghezza.
     Una parola da censurare viene passata dall'utente tramite parametro GET.
     Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
- -->
-
-<?php
+*/
 
 $chosenWord = (isset($_GET['badWord'])) ? $_GET['badWord'] : '';
 
+// Metodologia con EMPTY
+// if (!empty($_GET['badWord'])) {
+//     $chosenWord = $_GET['badWord'];
+// } else {
+//     $replacedParagraph = $originalParagraph;
+// }
+
 $originalParagraph = "Ogni operazione eseguita sul Web coinvolge un client ed un server. Un client è un dispositivo (ad esempio un browser) che effettua una richiesta ad un server remoto. Il server remoto attraverso un linguaggio di scripting (come per esempio PHP) interpreta la richiesta del client ed invia una risposta (ad esempio una pagina HTML, un oggetto JSON o un XML) al client. A questo punto il client è in grado di interpretare, a sua volta, la risposta ricevuta e fornirla all'utente; nel caso del browser riceverà una pagina HTML che mostrerà all'utente.";
 
+$originalLength = strlen($originalParagraph);
+
 $replacedParagraph = str_ireplace($chosenWord, '***', $originalParagraph);
+
+$replacedLength = strlen($replacedParagraph);
 
 ?>
 
@@ -32,6 +42,16 @@ $replacedParagraph = str_ireplace($chosenWord, '***', $originalParagraph);
         p {
             font-size: 1.5em;
         }
+
+        form {
+            width: 20%;
+            margin: 0 auto;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
     </style>
 
 </head>
@@ -45,7 +65,7 @@ $replacedParagraph = str_ireplace($chosenWord, '***', $originalParagraph);
 
         <h3>
             Lunghezza paragrafo:
-            <?php echo strlen($originalParagraph) ?>
+            <?php echo $originalLength ?>
         </h3>
 
         <p>
@@ -61,7 +81,7 @@ $replacedParagraph = str_ireplace($chosenWord, '***', $originalParagraph);
 
         <h3>
             Lunghezza paragrafo:
-            <?php echo strlen($replacedParagraph) ?>
+            <?php echo $replacedLength ?>
         </h3>
 
         <p>
@@ -71,7 +91,7 @@ $replacedParagraph = str_ireplace($chosenWord, '***', $originalParagraph);
     </div>
 
     <form method="get">
-        <label for="badWord">Inserisci la parola che vuoi sostituire</label>
+        <label for="badWord">Inserisci la parola che vuoi sostituire:</label>
         <input type="text" name="badWord">
         <button type="submit">Conferma</button>
     </form>
